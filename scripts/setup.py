e4dfef5db0294e79dc8ff45866c00df0ab1ada1b
Annemarie <solvingproblemsallthetime@gmail.com>
2025-11-17 22:44:50 +0000

    Create setup.py

---
+++ scripts/setup.py
@@ -0,0 +1,9 @@
+# content of/scripts/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="BOO_scripts", 
+    version="0.1",
+    packages=find_packages(),  # automatically find subpackages
+)
