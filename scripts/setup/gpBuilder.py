f7572e360007c80f685129877a694928e8ef393b
Annemarie <solvingproblemsallthetime@gmail.com>
2025-11-19 19:46:33 +0000

    Update gpBuilder.py

--- scripts/setup/gpBuilder.py
+++ scripts/setup/gpBuilder.py
@@ -3,19 +3,28 @@ from kernel_builder import build_kernel_from_config
 from defaultKernelSettings import DEFAULT_KERNEL_SETTINGS
 
 
-def build_gp(config=None, X_train=None, y_train=None, kernel_override=None):
+def build_gp(config=None, X_train=None, y_train=None, kernel_override=None, use_seed = True, seed= 42):
+ 
     input_dim = X_train.shape[1] if X_train is not None else None
     kernel = build_kernel_from_config(config=config, input_dim=input_dim, kernel_override=kernel_override)
 
     alpha = config.get("alpha", 1e-6) if config else DEFAULT_KERNEL_SETTINGS.get("white_noise", 1e-6)
     normalize_y = config.get("normalize_y", True) if config else True
     n_restarts_optimizer = config.get("n_restarts_optimizer", 5) if config else 5
-
-    gp = GaussianProcessRegressor(
-        kernel=kernel,
-        alpha=alpha,
-        normalize_y=normalize_y,
-        n_restarts_optimizer=n_restarts_optimizer
+    if use_seed:
+        gp = GaussianProcessRegressor(
+             kernel=kernel,
+             alpha=alpha,
+             normalize_y=normalize_y,
+             n_restarts_optimizer=n_restarts_optimizer
+             random_state=seed
+        )
+    else:
+        gp = GaussianProcessRegressor(
+            kernel=kernel,
+            alpha=alpha,
+            normalize_y=normalize_y,
+            n_restarts_optimizer=n_restarts_optimizer
     )
 
     if X_train is not None and y_train is not None:
